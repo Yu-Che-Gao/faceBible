64cde1c86edefc5b54d083e78bc503bd255a79f3
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-02-18 00:56:56 +0800

    基礎模型完成

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const title = 'FaceBible'
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/', function (req, res, next) {
+  res.render('index', { title: title })
+})
+
+router.get('/insert', function (req, res, next) {
+  res.render('insert', { title: title })
 })
 
 module.exports = router
